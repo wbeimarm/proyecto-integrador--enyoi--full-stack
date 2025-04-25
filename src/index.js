84c4506e188b46e9c77ed8e19bf1d65f9b64469b
wbeimarm <wmarmonsa@gmail.com>
2025-04-25 06:21:02 -0500

    Organizando proyecto antes de separar frontend y backend

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ const usuarioRoutes = require('./routes/usuariosRoutes');
 
 const app = express();
 
-// Middlewares
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['POST', 'GET'],
+    credentials: true,
+}));
+
 app.use(express.json());
-app.use(express.static('public')); // 🔥 Esta línea permite servir archivos estáticos desde la carpeta public
+app.use(express.static('public')); // permite servir archivos estáticos
 
 // Rutas API
 app.use('/api', usuarioRoutes);
@@ -18,7 +22,6 @@ app.get('/', (req, res) => {
     res.send('Servidor funcionando correctamente');
 });
 
-// Puerto
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
