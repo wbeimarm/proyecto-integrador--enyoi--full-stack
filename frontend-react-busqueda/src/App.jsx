7eabdc979b313cadd1e5ea85b176b54655d919f0
wbeimarm <wmarmonsa@gmail.com>
2025-04-25 13:49:58 -0500

    Eliminar carpeta frontend-react-busqueda del repositorio

--- frontend-react-busqueda/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import Login from './components/Login';
-
-function App() {
-  return <Login />;
-}
-
-export default App;
--- src/routes/usuariosRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { buscarUsuario } = require('../controllers/usuariosController') // esto importa el controlador
-
-// ruta que usa el controlador
-router.post('/buscarusuario', buscarUsuario);
-
-module.exports = router;
